c5e1cab5e0c6d5fe0812112efa0109c79a7ec336
imagisdev <76540537+imagisdev@users.noreply.github.com>
2020-12-27 10:07:50 -0500

    Added link to Heroku

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,7 @@
-<?php echo '<hr>Copyright ' . date('Y'); ?>
+<?php 
+    echo '<hr>';
+    echo '<a href="https://php-primer-img.herokuapp.com/" class="btn btn-danger">Heroku</a><br/>';
+    echo 'Copyright ' . date('Y'); ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </div>
 </body>
