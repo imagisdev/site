908525fcce2ca4b30800d7f9d8682042e1cae6ea
imagisdev <76540537+imagisdev@users.noreply.github.com>
2020-12-27 10:14:03 -0500

    Added link to local

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 <?php 
     echo '<hr>';
+    echo '<a href="http://localhost/site/" class="btn btn-primary">Local</a> ';
     echo '<a href="https://php-primer-img.herokuapp.com/" class="btn btn-danger">Heroku</a><br/>';
     echo 'Copyright ' . date('Y'); ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
